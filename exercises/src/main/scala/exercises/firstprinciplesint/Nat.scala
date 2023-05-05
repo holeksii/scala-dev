41aabccd23f5c728254de21c0d4ce400089071f3
Oleksii Hytsiv <oleksii.hytsiv@gmail.com>
2023-05-05 22:44:16 +0300

    Add natural number impl from the first principles

---
+++ exercises/src/main/scala/exercises/firstprinciplesint/Nat.scala
@@ -0,0 +1,8 @@
+package exercises.firstprinciplesint
+
+abstract class Nat:
+  def isZero: Boolean
+  def predecessor: Nat
+  def successor: Nat
+  def +(that: Nat): Nat
+  def -(that: Nat): Nat
---
+++ exercises/src/main/scala/exercises/firstprinciplesint/sandbox.worksheet.sc
@@ -0,0 +1,8 @@
+import exercises.firstprinciplesint.*
+
+val one = Succ(Zero)
+val two = Succ(one)
+
+one + two
+one + Zero
+one - one
